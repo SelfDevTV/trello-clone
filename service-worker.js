b31adc87d3689c2f0a7b703344bb2e6370ff550a
Huber Dominik <Huberdo@voestalpine.root.local>
2019-03-27 15:48:59 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/trello-clone/precache-manifest.263826be5b7c3562c2bff3e3b2d008fb.js"
+  "/trello-clone/precache-manifest.6435d90826e975a3a6d224fec428e46d.js"
 );
 
 workbox.clientsClaim();
