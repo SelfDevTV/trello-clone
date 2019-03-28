1c98cbe5bd7cb2b1fdc9fe1063556083818e170f
Huber Dominik <Huberdo@voestalpine.root.local>
2019-03-28 17:53:51 +0100

    switched to hash router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import TrelloBoard from "../components/TrelloBoard";
 import Home from "../components/Home";
 
